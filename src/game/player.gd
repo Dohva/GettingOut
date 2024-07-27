66a9eb9aac254eb083d6fea47c998d0e2b9707c4
joachimhviid <joac1099@gmail.com>
2024-07-27 12:41:10 +0200

    Updated tilemap for autotiling

--- src/game/player.gd
+++ src/game/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var animated_sprite = $AnimatedSprite2D
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -400.0
 
 enum State { Idle, Run, Jump }
 
