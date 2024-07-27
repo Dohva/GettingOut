5891d14208259a13f968129ea943da445166a833
joachimhviid <joac1099@gmail.com>
2024-07-27 10:55:29 +0200

    Revert test commits

--- src/game/player.gd
+++ src/game/player.gd
@@ -9,8 +9,6 @@ enum State { Idle, Run, Jump }
 
 var current_state: State = State.Idle
 
-var test = "git commit3";
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
